9d7a840698a7afb2d51080ccee1f40a465d98815
OtakuBoy420 <U28282@rts-365.dk>
2022-02-02 14:21:05 +0100

    onsdag

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// function myFunction() {
+//     var list = document.getElementsByClassName('navigation__list')[0];
+//     if (list.style.display == "flex") {
+//       list.style.display = "none";
+//     } else {
+//       list.style.display = "flex";
+//     }
+//   }
