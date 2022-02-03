d0985b7a9876127e6ee6986862abd5995491adc9
OtakuBoy420 <U28282@rts-365.dk>
2022-02-03 14:22:13 +0100

    torsdag

---
+++ assets/js/slider.js
@@ -0,0 +1,44 @@
+const sliderContainer = document.getElementsByClassName("slider")[0];
+const prevBtn = sliderContainer.querySelector(".prevBtn")
+const nextBtn = sliderContainer.querySelector(".nextBtn")
+var index = 0
+
+const images = [
+    "assets/img/pic1.jpg",
+    "assets/img/pic2.jpg",
+    "assets/img/pic3.png"
+]
+
+updateSlider();
+
+prevBtn.addEventListener("click", showPrev)
+nextBtn.addEventListener("click", showNext)
+
+function showNext() {
+    if (index < images.length - 1) {
+        index = index + 1
+    } else {
+        index = 0
+    }
+    updateSlider()
+}
+
+function showPrev() {
+    if (index > 0) {
+        index = index - 1
+    } else {
+        index = images.length - 1
+    }
+    updateSlider()
+}
+
+function updateSlider() {
+    const image = sliderContainer.querySelector(".slider__img")
+
+    image.classList.toggle("slider__img--fade")
+    setTimeout(function () {
+        image.src = images[index]
+        image.classList.toggle("slider__img--fade")
+    }, 100)
+
+}
