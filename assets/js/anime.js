9b4343592b9bd63c80356883fd330bc81ba39e06
OtakuBoy420 <U28282@rts-365.dk>
2022-03-02 14:17:13 +0100

    fix

--- assets/js/anime.js
+++ assets/js/anime.js
@@ -1,37 +1,42 @@
-const API_URL = 'https://api.jikan.moe/v4/anime?order_by=members&sort=desc&offset=0';
-const API_URL2 = 'https://api.jikan.moe/v4/anime?order_by=members&sort=desc&offset=0&page=2';
+const limit = 24
+    const queries = new URLSearchParams(window.location.search)
+    const currentPage = queries.get("page")
+   const API_URL = `https://api.jikan.moe/v4/anime?order_by=members&sort=desc&limit=${limit}&page=${currentPage}`; 
 
 async function getSingleAnime(anime) {
     let response = await fetch(API_URL);
     let json = await response.json();
+    console.log(json, anime);
     let singleAnime = json.data.find(element => element.title_english === anime)
-    document.title += " - " + singleAnime.title
+    document.title += " - " + singleAnime.title_english
     console.log(singleAnime);
-    const NEW_ITEM = `<article>
+    const NEW_ITEM = `
             <h1>${singleAnime.title_english}</h1>
-            <p>${singleAnime.title_japanese}</p>
-            <p>${singleAnime.year}</p>
-            <a class="trailerLink" href="#trailer">Watch Trailer</a>
-            <img src="${singleAnime.images.webp.large_image_url}">
-            <p>${singleAnime.synopsis}</p>
-            <p>Aired from: ${singleAnime.aired.prop.from.year} - ${singleAnime.aired.prop.to.year}</p>
-            <p>Licensor: ${singleAnime.licensors[0].name}</p>
-            <p>Producers: ${singleAnime.producers.map(producers => ' ' + producers.name)}</p>
-            <p>${singleAnime.score}⭐</p>
-            <a class="animeLink" href="${singleAnime.url}">MAL LINK</a>
-            <p>Episodes: ${singleAnime.episodes}</p>
-            <p></p>
-            </article>    
-            <div class="trailer">
+            <p class="singleAnimeInfo">${singleAnime.title_japanese}</p>
+            <p class="singleAnimeInfo">${singleAnime.year}</p>
+            <img class="singleAnimeImg" src="${singleAnime.images.webp.large_image_url}">
+            <a class="trailerLink" href="#trailer" onclick="popUpTrailer();"> ▶Watch Trailer</a>
+            <h2 class="singleAnimeInfo">Synopsis:</h2>
+            <p class="synopsis">${singleAnime.synopsis}</p>
+            <p class="singleAnimeInfo">Rated: ${singleAnime.score}⭐</p>
+            <p class="singleAnimeInfo">Episodes: ${singleAnime.episodes}</p>
+            <a class="animeLink singleAnimeInfo" href="${singleAnime.url}" target="_blank">MAL LINK</a>
+            <p class="singleAnimeInfo">Aired from: ${singleAnime.aired.prop.from.year} - ${singleAnime.aired.prop.to.year}</p>
+            <p class="singleAnimeInfo">Licensor: ${singleAnime.licensors[0].name}</p>
+            <p class="singleAnimeInfo">Producers: ${singleAnime.producers.map(producers => ' ' + producers.name)}</p>    
+            <div class="modal-outer">
+                <div class="modal-inner">
                 <iframe width="470" height="315" src="${singleAnime.trailer.embed_url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <button class="close-modal">X</button>
+                </div>
             </div>
             `
-    let singleApi = document.getElementsByClassName("singleApi")[0]
-    singleApi.innerHTML += NEW_ITEM
+    let singleAnimeElement = document.getElementsByClassName("singleAnime")[0]
+    singleAnimeElement.innerHTML += NEW_ITEM
 }
 
 
-const queries = new URLSearchParams(window.location.search)
+
 const anime = queries.get("anime")
 if (!anime) {
     window.location.href = "/404.html"
@@ -39,10 +44,12 @@ if (!anime) {
     getSingleAnime(anime)
 }
 
-let animeLink = document.querySelector('.animeLink');
 
-
-    animeLink.addEventListener("click", function(event){
-        event.preventDefault();
-
+function popUpTrailer(event) {
+    let closeBtn = document.querySelector(".close-modal");
+    let modalOuter = document.querySelector(".modal-outer");
+    modalOuter.classList.add("open"); 
+    modalOuter.addEventListener("click", function(event){
+        modalOuter.classList.remove("open")
     });
+}
--- assets/js/api.js
+++ assets/js/api.js
@@ -1,17 +1,41 @@
-const API_URL = 'https://api.jikan.moe/v4/anime?order_by=members&sort=desc&offset=0';
-const API_URL2 = 'https://api.jikan.moe/v4/anime?order_by=members&sort=desc&offset=0&page=2';
+const limit = 24
+const queries = new URLSearchParams(window.location.search)
+var currentPage = parseInt(queries.get("page")) || 1
+const API_URL = `https://api.jikan.moe/v4/anime?order_by=members&sort=desc&limit=${limit}&page=${currentPage}`;
+
+var prevBtn = document.querySelector('.prev');
+var nextBtn = document.querySelector('.next');
 
 async function getAnime() {
     let response = await fetch(API_URL);
     let json = await response.json();
+    const pages = Math.ceil(23593 / limit)
+    if (!json.meta){
+        Math.ceil(23593 / limit)
+    } else{
+        Math.ceil(json.meta.total / limit)
+    }
+    
+    prevBtn.href = `?page=${currentPage - 1}`
+    nextBtn.href = `?page=${currentPage + 1}`
+    if (currentPage > pages) {
+        currentPage = pages
+        prevBtn.href = `?page=${currentPage - 1}`
+        nextBtn.href = `?page=${currentPage}`
+    }
+    
+    if (currentPage < 1) {
+        currentPage = 1
+        prevBtn.href = `?page=${currentPage}`
+        nextBtn.href = `?page=${currentPage + 1}`
+    }
+    console.log(pages, currentPage, json);
 
-    let i = 1;
-    console.log(json);
     json.data.forEach(element => {
         const NEW_ITEM = `
-        <article class="apiArticle" onclick="window.location.href='/anime.html?anime=${element.title_english}'">
+        <article class="apiArticle" onclick="window.location.href='/anime.html?anime=${element.title_english}&page=${currentPage}'">
         <img class="apiImg" src="${element.images.webp.large_image_url}" alt ="${element.title_english} Image">
-        <h1 class="apiHeading"><a class="apiHeading__link" href="/anime.html?anime=${element.title_english}" target="_blank">${i}. ${element.title_english}</a></h1>
+        <h1 class="apiHeading"><a class="apiHeading__link" href="/anime.html?anime=${element.title_english}" target="_blank">${element.popularity}. ${element.title_english}</a></h1>
         <h2 class="apiSecondHeading">${element.title_japanese}</h2>
         <p class="apiScore">Score: ${element.score}⭐</p>
         <p class="apiScore">Members: ${element.members}</p>
@@ -19,28 +43,11 @@ async function getAnime() {
     `
         let apiElements = document.querySelector('.apiElements');
         apiElements.innerHTML += NEW_ITEM
-        i++
     })
 
 
 
-    let response2 = await fetch(API_URL2);
-    let json2 = await response2.json();
-    console.log(json2);
-    json2.data.forEach(element => {
-        const NEW_ITEM = `
-        <article class="apiArticle" onclick="window.location.href='/anime.html?anime=${element.title_english}'">
-        <img class="apiImg" src="${element.images.webp.large_image_url}" alt ="${element.title_english} Image">
-        <h1 class="apiHeading"><a class="apiHeading__link" href="/anime.html?anime=${element.title_english}" target="_blank">${i}. ${element.title_english}</a></h1>
-        <h2 class="apiSecondHeading">${element.title_japanese}</h2>
-        <p class="apiScore">Score: ${element.score}⭐</p>
-        <p class="apiScore">Members: ${element.members}</p>
-        </article>
-    `
-        let apiElements = document.querySelector('.apiElements');
-        apiElements.innerHTML += NEW_ITEM
-        i++
-    })
+
 
 
 let searchBar = document.querySelector('#searchBar');
@@ -57,4 +64,35 @@ searchBar.addEventListener('input', function () {
     })
 });
 
+let pageIdentifier = document.querySelector('.pageIdentifier');
+pageIdentifier.innerHTML = "Page: " + currentPage
+
 getAnime()
+
+
+
+
+
+
+
+
+
+// const API_URL2 = 'https://api.jikan.moe/v4/anime?order_by=members&sort=desc&offset=0&page=2';    
+
+// let response2 = await fetch(API_URL2);
+    // let json2 = await response2.json();
+    // console.log(json2);
+    // json2.data.forEach(element => {
+    //     const NEW_ITEM = `
+    //     <article class="apiArticle" onclick="window.location.href='/anime.html?anime=${element.title_english}'">
+    //     <img class="apiImg" src="${element.images.webp.large_image_url}" alt ="${element.title_english} Image">
+    //     <h1 class="apiHeading"><a class="apiHeading__link" href="/anime.html?anime=${element.title_english}" target="_blank">${i}. ${element.title_english}</a></h1>
+    //     <h2 class="apiSecondHeading">${element.title_japanese}</h2>
+    //     <p class="apiScore">Score: ${element.score}⭐</p>
+    //     <p class="apiScore">Members: ${element.members}</p>
+    //     </article>
+    // `
+    //     let apiElements = document.querySelector('.apiElements');
+    //     apiElements.innerHTML += NEW_ITEM
+    //     i++
+    // })
